03f493ed332b32ab513bc9f28d1822d07e29c69b
Joshua <raoyunxuan@outlook.com>
2023-11-02 00:51:01 -0700

    3.1 almost done, need styling for bottons

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,9 +1,34 @@
 import db from "../Database";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaEdit } from "react-icons/fa";
 import "./index.css";
 function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+  });
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
 
   return (
     <div className="ps-3">
@@ -14,9 +39,35 @@ function Dashboard() {
         <hr />
       </h2>
 
+      <h5>Edit Course</h5>
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.startDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+      />
+      <input
+        value={course.endDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+      />
+      <button onClick={addNewCourse}>Add</button>
+      <button onClick={updateCourse}>Update</button>
+
       <div className="ps-3 d-flex flex-row">
         <div className="wd-kanbas card-container">
-          {courses.map((course, index) => (
+          {courses.map((course) => (
             <div>
               <div>
                 <div className="card wd-kanbas card-size">
@@ -31,6 +82,23 @@ function Dashboard() {
                       style={{ textDecoration: "none" }}
                     >
                       <h5>{course.name}</h5>
+                      <button
+                        onClick={(event) => {
+                          event.preventDefault();
+                          setCourse(course);
+                        }}
+                      >
+                        Edit
+                      </button>
+
+                      <button
+                        onClick={(event) => {
+                          event.preventDefault();
+                          deleteCourse(course._id);
+                        }}
+                      >
+                        Delete
+                      </button>
                     </Link>
                     <div className="card-text">
                       CS4550.12631.202410
