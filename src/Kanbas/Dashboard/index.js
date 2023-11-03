da0bb32139e9e89a724a758cc66fbfa8412382f6
Joshua <raoyunxuan@outlook.com>
2023-11-02 18:20:42 -0700

    hw4 doen

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -3,68 +3,78 @@ import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaEdit } from "react-icons/fa";
 import "./index.css";
-function Dashboard() {
-  const [courses, setCourses] = useState(db.courses);
-  const [course, setCourse] = useState({
-    name: "New Course",
-    number: "New Number",
-    startDate: "2023-09-10",
-    endDate: "2023-12-15",
-  });
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
-  };
-
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
-  };
-  const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
-  };
-
+function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}) {
   return (
-    <div className="ps-3">
+    <div className="ps-3 ">
       <h1>Dashboard</h1>
       <hr />
       <h2 className="ps-3">
         Published Courses ({courses.length})
         <hr />
       </h2>
-
-      <h5>Edit Course</h5>
-      <input
-        value={course.name}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, name: e.target.value })}
-      />
-      <input
-        value={course.number}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <input
-        value={course.startDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
-      />
-      <input
-        value={course.endDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
-      />
-      <button onClick={addNewCourse}>Add</button>
-      <button onClick={updateCourse}>Update</button>
-
+      <ul className="wd-kanbas card-container">
+        <h5>Edit Course:</h5>
+      </ul>
+      <ul className="wd-kanbas card-container">
+        <div className="d-flex">
+          <input
+            value={course.name}
+            className="form-control"
+            onChange={(e) => setCourse({ ...course, name: e.target.value })}
+          />
+          <input
+            value={course.number}
+            className="form-control ms-3"
+            onChange={(e) => setCourse({ ...course, number: e.target.value })}
+          />
+          <input
+            value={course.startDate}
+            className="form-control ms-3"
+            type="date"
+            onChange={(e) =>
+              setCourse({ ...course, startDate: e.target.value })
+            }
+          />
+          <input
+            value={course.endDate}
+            className="form-control ms-3"
+            type="date"
+            onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+          />
+        </div>
+        <div className="d-flex float-end">
+          <button
+            className="btn btn-primary"
+            style={{
+              backgroundColor: "green",
+              border: "none",
+              color: "white",
+              padding: "5px 10px",
+            }}
+            onClick={addNewCourse}
+          >
+            Add
+          </button>
+          <button
+            className="btn btn-primary ms-2"
+            style={{
+              border: "none",
+              color: "white",
+              padding: "5px 10px",
+            }}
+            onClick={updateCourse}
+          >
+            Update
+          </button>
+        </div>
+      </ul>
       <div className="ps-3 d-flex flex-row">
         <div className="wd-kanbas card-container">
           {courses.map((course) => (
@@ -83,6 +93,13 @@ function Dashboard() {
                     >
                       <h5>{course.name}</h5>
                       <button
+                        className="btn btn-primary"
+                        style={{
+                          backgroundColor: "orange",
+                          border: "none",
+                          color: "black",
+                          padding: "5px 10px",
+                        }}
                         onClick={(event) => {
                           event.preventDefault();
                           setCourse(course);
@@ -92,6 +109,13 @@ function Dashboard() {
                       </button>
 
                       <button
+                        className="btn btn-primary ms-1"
+                        style={{
+                          backgroundColor: "red",
+                          border: "none",
+                          color: "white",
+                          padding: "5px 10px",
+                        }}
                         onClick={(event) => {
                           event.preventDefault();
                           deleteCourse(course._id);
