6cd164ec3bd93ffcee6c40b3acc34bf43cb7e870
Joshua <raoyunxuan@outlook.com>
2023-11-04 23:46:25 -0700

    a5 lab 3.1 done

---
+++ kanbas-react-web-app/src/Labs/a5/index.js
@@ -0,0 +1,15 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+function Assignment5() {
+  return (
+    <div className="container">
+      <h1>Assignment 5</h1>
+      <div className="list-group">
+        <a href="http://localhost:4000/a5/welcome" className="list-group-item">
+          Welcome
+        </a>
+        <EncodingParametersInURLs />
+      </div>
+    </div>
+  );
+}
+export default Assignment5;
