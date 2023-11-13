6bf39529b579460d7ed79c3efd5de1349fe6410b
Joshua <raoyunxuan@outlook.com>
2023-11-13 13:54:48 -0800

    4.2.1 done

--- kanbas-react-web-app/src/Kanbas/index.js
+++ kanbas-react-web-app/src/Kanbas/index.js
@@ -3,19 +3,29 @@ import KanbasNavigation from "./KanbasNavigation";
 import Courses from "./Courses";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
-import db from "./Database";
-import { useState } from "react";
+//import db from "./Database";
+import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 function Kanbas() {
-  const [courses, setCourses] = useState(db.courses);
+  const [courses, setCourses] = useState([]);
   const [course, setCourse] = useState({
     name: "New Course",
     number: "New Number",
     startDate: "2023-09-10",
     endDate: "2023-12-15",
   });
+  const URL = "http://localhost:4000/api/courses";
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
+  };
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+
   const addNewCourse = () => {
     setCourses([
       ...courses,
